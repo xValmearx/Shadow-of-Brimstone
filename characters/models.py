108310863acee65145e43f8dda1cb7d83de32327
caleb <calebs.phone2017@gmail.com>
2025-06-11 22:55:25 -0400

    finished creating character auto fill selection

--- characters/models.py
+++ characters/models.py
@@ -14,7 +14,14 @@ class Character(models.Model):
 
     class_options = (
         ("Marshal", "Marshal"),
-        ("Rager", "Ranger"),
+        ("Rancher", "Rancher"),
+        ("LawMan","LawMan"),
+        ("PianoPlayer/SaloonGirl","PianoPlayer/SaloonGirl"),
+        ("IndianScout","IndianScout"),
+        ("Bandido","Bandido"),
+        ("Preacher","Preacher"),
+        ("GunSlinger","GunSlinger"),
+
     )
 
     character_class = models.CharField(choices=class_options, default="Marshal")
@@ -22,31 +29,238 @@ class Character(models.Model):
     # user gets to name the character
     character_name = models.CharField(max_length=25,blank=False)
 
-    agility = models.IntegerField(default=3)
-    cunning = models.IntegerField(default=4)
-    spirit = models.IntegerField(default=2)
-    strength = models.IntegerField(default=2)
-    lore = models.IntegerField(default=1)
-    luck = models.IntegerField(default=3)
-
-    initiative = models.IntegerField(default=4)
-
-    range_to_hit = models.IntegerField(default=4)
-    melee_to_hit = models.IntegerField(default=4)
-    combat = models.IntegerField(default=2)
-
-    max_grit = models.IntegerField(default=2)
-
-    health = models.IntegerField(default=10)
-    defence = models.IntegerField(default=3)
-
-    sanity = models.IntegerField(default=10)
-    willpower = models.IntegerField(default=4)
-
     @property
+    def agility(self):
+        if self.character_class == "Marshal":
+            return 3
+        if self.character_class == "Rancher":
+            return 2
+        if self.character_class == "LawMan":
+            return 2
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 4
+        if self.character_class == "IndianScout":
+            return 3
+        if self.character_class == "Bandido":
+            return 2
+        if self.character_class == "Preacher":
+            return 1
+        if self.character_class == "GunSlinger":
+            return 3
+    def cunning(self):
+        if self.character_class == "Marshal":
+            return 4
+        if self.character_class == "Rancher":
+            return 2
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 3
+        if self.character_class == "IndianScout":
+            return 2
+        if self.character_class == "Bandido":
+            return 1
+        if self.character_class == "Preacher":
+            return 2
+        if self.character_class == "GunSlinger":
+            return 3
+    def spirit(self):
+        if self.character_class == "Marshal":
+            return 2
+        if self.character_class == "Rancher":
+            return 3
+        if self.character_class == "LawMan":
+            return 1
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 3
+        if self.character_class == "IndianScout":
+            return 3
+        if self.character_class == "Bandido":
+            return 3
+        if self.character_class == "Preacher":
+            return 4
+        if self.character_class == "GunSlinger":
+            return 2
+    def strength(self):
+        if self.character_class == "Marshal":
+            return 2
+        if self.character_class == "Rancher":
+            return 3
+        if self.character_class == "LawMan":
+            return 3
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 1
+        if self.character_class == "IndianScout":
+            return 2
+        if self.character_class == "Bandido":
+            return 4
+        if self.character_class == "Preacher":
+            return 3
+        if self.character_class == "GunSlinger":
+            return 2
+    def lore(self):
+        if self.character_class == "Marshal":
+            return 1
+        if self.character_class == "Rancher":
+            return 4
+        if self.character_class == "LawMan":
+            return 2
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 2
+        if self.character_class == "IndianScout":
+            return 3
+        if self.character_class == "Bandido":
+            return 3
+        if self.character_class == "Preacher":
+            return 3
+        if self.character_class == "GunSlinger":
+            return 2
+    def luck(self):
+        if self.character_class == "Marshal":
+            return 3
+        if self.character_class == "Rancher":
+            return 1
+        if self.character_class == "LawMan":
+            return 3
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 3
+        if self.character_class == "IndianScout":
+            return 2
+        if self.character_class == "Bandido":
+            return 2
+        if self.character_class == "Preacher":
+            return 2
+        if self.character_class == "GunSlinger":
+            return 4
+    def initiative(self):
+        if self.character_class == "Marshal":
+            return 4
+        if self.character_class == "Rancher":
+            return 3
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 5
+        if self.character_class == "IndianScout":
+            return 5
+        if self.character_class == "Bandido":
+            return 3
+        if self.character_class == "Preacher":
+            return 2
+        if self.character_class == "GunSlinger":
+            return 6
+    def range_to_hit(self):
+        if self.character_class == "Marshal":
+            return 4
+        if self.character_class == "Rancher":
+            return 4
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 4
+        if self.character_class == "IndianScout":
+            return 4
+        if self.character_class == "Bandido":
+            return 5
+        if self.character_class == "Preacher":
+            return 5
+        if self.character_class == "GunSlinger":
+            return 3
+    def melee_to_hit(self):
+        if self.character_class == "Marshal":
+            return 4
+        if self.character_class == "Rancher":
+            return 4
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 4
+        if self.character_class == "IndianScout":
+            return 4
+        if self.character_class == "Bandido":
+            return 4
+        if self.character_class == "Preacher":
+            return 4
+        if self.character_class == "GunSlinger":
+            return 5
+        
+    def combat(self):
+        if self.character_class == "GunSlinger":
+            return 1
+        else:
+            return 2
+    def health(self):
+        if self.character_class == "Marshal":
+            return 10
+        if self.character_class == "Rancher":
+            return 14
+        if self.character_class == "LawMan":
+            return 12
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 8
+        if self.character_class == "IndianScout":
+            return 10
+        if self.character_class == "Bandido":
+            return 16
+        if self.character_class == "Preacher":
+            return 12
+        if self.character_class == "GunSlinger":
+            return 10
+    def defence(self):
+        if self.character_class == "Marshal":
+            return 3
+        if self.character_class == "Rancher":
+            return 4
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 3
+        if self.character_class == "IndianScout":
+            return 4
+        if self.character_class == "Bandido":
+            return 4
+        if self.character_class == "Preacher":
+            return 5
+        if self.character_class == "GunSlinger":
+            return 5
+    def sanity(self):
+        if self.character_class == "Marshal":
+            return 10
+        if self.character_class == "Rancher":
+            return 10
+        if self.character_class == "LawMan":
+            return 12
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 14
+        if self.character_class == "IndianScout":
+            return 10
+        if self.character_class == "Bandido":
+            return 8
+        if self.character_class == "Preacher":
+            return 10
+        if self.character_class == "GunSlinger":
+            return 12
+    def willpower(self):
+        if self.character_class == "Marshal":
+            return 4
+        if self.character_class == "Rancher":
+            return 4
+        if self.character_class == "LawMan":
+            return 4
+        if self.character_class == "PianoPlayer/SaloonGirl":
+            return 4
+        if self.character_class == "IndianScout":
+            return 4
+        if self.character_class == "Bandido":
+            return 5
+        if self.character_class == "Preacher":
+            return 3
+        if self.character_class == "GunSlinger":
+            return 4
     def weight(self):
-        return 5 + self.strength
-
+        return 5 + self.strength()
+    
+    max_grit = models.IntegerField(default=2)
     def __str__(self):
         return self.character_name
     
